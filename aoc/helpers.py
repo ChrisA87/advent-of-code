8307109d789f62bcfe08852f6ec523558309c7b1
Chris Adams <cadams@getaroom.com>
2022-12-02 00:40:21 +0000

    initial commit

---
+++ aoc/helpers.py
@@ -0,0 +1,13 @@
+import os
+import requests
+
+SESSION_ID = os.environ['AOC_SESSION']
+BASE_URL = 'https://adventofcode.com/{}/day/{}/input'
+
+def get_input_data(day, year=2022):
+    url = BASE_URL.format(year, day)
+    res = requests.get(url, headers={'cookie': SESSION_ID})
+    data = res.content.decode().strip()
+    if res.status_code == 200:
+        return data
+    raise requests.HTTPError(data)

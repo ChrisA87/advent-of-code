1e99002d70e13ceb97cc08c98e8d4de351399b7f
Chris Adams <cadams@getaroom.com>
2022-12-05 08:55:34 +0000

    updated solution 2022 - day 5 part 1

--- aoc/helpers.py
+++ aoc/helpers.py
@@ -7,7 +7,7 @@ BASE_URL = 'https://adventofcode.com/{}/day/{}/input'
 def get_input_data(day, year=2022):
     url = BASE_URL.format(year, day)
     res = requests.get(url, headers={'cookie': SESSION_ID})
-    data = res.content.decode().strip()
+    data = res.content.decode().rstrip()
     if res.status_code == 200:
         return data
     raise requests.HTTPError(data)

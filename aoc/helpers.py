45d20def79b0eeb594bed1bd94c5fd8ca08c2cd8
Chris Adams <cfh1990@msn.com>
2023-12-02 18:20:44 +0000

    updates default year to 2023

--- aoc/helpers.py
+++ aoc/helpers.py
@@ -1,12 +1,13 @@
 import os
 import requests
 
-SESSION_ID = os.environ['AOC_SESSION']
-BASE_URL = 'https://adventofcode.com/{}/day/{}/input'
+SESSION_ID = os.environ["AOC_SESSION"]
+BASE_URL = "https://adventofcode.com/{}/day/{}/input"
 
-def get_input_data(day, year=2022):
+
+def get_input_data(day, year=2023):
     url = BASE_URL.format(year, day)
-    res = requests.get(url, headers={'cookie': SESSION_ID})
+    res = requests.get(url, headers={"cookie": SESSION_ID})
     data = res.content.decode().rstrip()
     if res.status_code == 200:
         return data

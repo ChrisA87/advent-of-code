652fda3dfe6913a292657344789d5336e3a7c246
Chris Adams <cfh1990@msn.com>
2023-12-10 14:36:30 +0000

    adds day 9 part 2

--- src/aoc/2023/09.py
+++ src/aoc/2023/09.py
@@ -21,7 +21,7 @@ class Report:
             )
         )
 
-    def extrapolate(self):
+    def extrapolate_right(self):
         vals = []
         arr = self.values
 
@@ -30,20 +30,45 @@ class Report:
             vals.append(arr[-1])
         return self.values[-1] + sum(vals)
 
+    def extrapolate_left(self):
+        arr = self.values
+        vals = []
+        i = 0
+        while not np.all(arr == 0):
+            arr = np.diff(arr)
+            i += 1
+            vals.append(arr[0])
+
+        val = 0
+        while vals:
+            val = vals.pop() - val
+        return self.values[0] - val
+
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(values={self.values})"
 
 
+@aoc.part(1)
 def solution_1():
     extrapolated_values = []
     for line in data.split("\n"):
         report = Report.from_text(line)
-        extrapolated_values.append(report.extrapolate())
+        extrapolated_values.append(report.extrapolate_right())
+    return sum(extrapolated_values)
+
+
+@aoc.part(2)
+def solution_2():
+    extrapolated_values = []
+    for line in data.split("\n"):
+        report = Report.from_text(line)
+        extrapolated_values.append(report.extrapolate_left())
     return sum(extrapolated_values)
 
 
 def main():
-    print(solution_1())
+    print(solution_1())  # 1901217887
+    print(solution_2())
 
 
 if __name__ == "__main__":

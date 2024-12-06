386341d7174a94aaa127c5b314a421b14cedc186
Chris Adams <Chris.Adams@lmrpartners.com>
2024-12-06 15:20:38 +0000

    day 6

--- src/aoc/2024/06.py
+++ src/aoc/2024/06.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+from tqdm import tqdm
 
 from dataclasses import dataclass
 import aoc
@@ -24,6 +25,7 @@ moves = {
     "<": lambda i, j: (i, j - 1),
 }
 
+class LoopError(Exception): ...
 
 @dataclass
 class Guard:
@@ -31,14 +33,26 @@ class Guard:
     direction: str
 
     @property
-    def next_position(self) -> Tuple[int, int]:
+    def position_ahead(self) -> Tuple[int, int]:
         return moves.get(self.direction)(*self.position)
+
+    def get_next_position(self):
+        return Guard(self.position_ahead, self.direction)
 
     def turn(self):
         self.direction = directions.get(self.direction)
 
     def move(self):
-        self.position = self.next_position
+        self.position = self.position_ahead
+
+    def copy(self):
+        return Guard(self.position, self.direction)
+
+    def __eq__(self, other):
+        return self.position == other.position and self.direction == other.direction
+
+    def __hash__(self):
+        return hash((self.position, self.direction))
 
 
 def parse_data(data: str):
@@ -46,38 +60,78 @@ def parse_data(data: str):
     for i, row in enumerate(grid):
         for j, val in enumerate(row):
             if val in directions:
-                position = Guard((i, j), val)
-                return grid, position
+                guard = Guard((i, j), val)
+                return grid, guard
 
 
-def in_bounds(position: Tuple[int, int], grid: List[str]) -> bool:
+def in_bounds(guard: Guard, grid: List[str]) -> bool:
     rows, col = len(grid), len(grid[0])
-    i, j = position
+    i, j = guard.position
     return (0 <= i < rows) and (0 <= j < col)
 
 
-def obstructed(position: Tuple[int, int], grid: List[str]) -> bool:
-    i, j = position
+def obstructed(guard: Guard, grid: List[str]) -> bool:
+    i, j = guard.position
     return grid[i][j] == "#"
+
+
+def get_route(guard: Guard, grid: List[str]):
+    route = [guard.copy()]
+
+    while in_bounds(next_position := guard.get_next_position(), grid):
+        if not obstructed(next_position, grid):
+            guard.move()
+        else:
+            guard.turn()
+        # Already been here
+        if guard in route:
+            raise LoopError("Loop deteced")
+
+        route.append(guard.copy())
+
+    return route
+
+
+def insert_obstruction(grid, position):
+    i, j = position
+    grid = grid[:]
+    row = grid[i]
+    grid[i] = row[:j] + "#" + row[j + 1 :]
+    return grid
 
 
 @aoc.part(1)
 def part_1() -> int:
     grid, guard = parse_data(data)
-    seen = {guard.position}
-
-    while in_bounds(guard.next_position, grid):
-        if not obstructed(guard.next_position, grid):
-            guard.move()
-            seen.add(guard.position)
-        else:
-            guard.turn()
-    return len(seen)
+    route = get_route(guard, grid)
+    return len(set(p.position for p in route))
 
 
 @aoc.part(2)
 def part_2() -> int:
-    pass
+    grid, guard = parse_data(data)
+
+    route = get_route(guard, grid)
+    checked_obstructions = set()
+    seen_positions = set()
+    caused_loop = set()
+
+    for starting_state in tqdm(route):
+        if (
+            ahead := starting_state.position_ahead
+        ) in checked_obstructions or starting_state in seen_positions:
+            continue
+
+        checked_obstructions.add(ahead)
+        seen_positions.add(starting_state)
+        try:
+            get_route(starting_state, insert_obstruction(grid, ahead))
+        except IndexError:
+            continue
+        except LoopError:
+            caused_loop.add(ahead)
+
+    return len(caused_loop - {route[0].position})
 
 
 def main():

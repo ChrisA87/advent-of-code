4dd2fc4e391a30470e944508b4e1ed247cfa4083
Chris Adams <cfh1990@msn.com>
2024-12-07 06:32:36 +0000

    add template

---
+++ src/aoc/template.py
@@ -0,0 +1,24 @@
+import aoc
+
+data = """"""
+
+# data = aoc.get_data_from_file()
+
+
+@aoc.part(1)
+def part_1() -> int:
+    pass
+
+
+@aoc.part(2)
+def part_2() -> int:
+    pass
+
+
+def main():
+    print(part_1())
+    print(part_2())
+
+
+if __name__ == "__main__":
+    main()

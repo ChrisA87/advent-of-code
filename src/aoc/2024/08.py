34ac5228a0f7a6ae95374a19db406e67891c39dc
Chris Adams <cfh1990@msn.com>
2024-12-08 10:19:22 +0000

    day 8 part 2

--- src/aoc/2024/08.py
+++ src/aoc/2024/08.py
@@ -38,12 +38,34 @@ def is_valid(antinode: Position):
     return (0 <= antinode.x < ROWS) and (0 <= antinode.y < COLS)
 
 
+def get_antinode(position: Position, x_diff: int, y_diff: int):
+    return Position(position.x - x_diff, position.y - y_diff)
+
+
 def get_valid_antinodes(position1: Position, position2: Position):
     x_diff = position2.x - position1.x
     y_diff = position2.y - position1.y
-    antinode1 = Position(position1.x - x_diff, position1.y - y_diff)
-    antinode2 = Position(position2.x + x_diff, position2.y + y_diff)
+    antinode1 = get_antinode(position1, x_diff, y_diff)
+    antinode2 = get_antinode(position2, -x_diff, -y_diff)
     return [antinode for antinode in [antinode1, antinode2] if is_valid(antinode)]
+
+
+def get_valid_antinodes_v2(position1: Position, position2: Position):
+
+    x_diff = position2.x - position1.x
+    y_diff = position2.y - position1.y
+    antinodes1 = [position1]
+    antinodes2 = [position2]
+
+    while is_valid(antinode := get_antinode(position1, x_diff, y_diff)):
+        antinodes1.append(antinode)
+        position1 = antinode
+
+    while is_valid(antinode := get_antinode(position2, -x_diff, -y_diff)):
+        antinodes2.append(antinode)
+        position2 = antinode
+
+    return antinodes1 + antinodes2
 
 
 @aoc.part(1)
@@ -57,7 +79,11 @@ def part_1(antena_positions: Dict[str, List[Tuple[int, int]]]) -> int:
 
 @aoc.part(2)
 def part_2(antena_positions: Dict[str, List[Tuple[int, int]]]) -> int:
-    pass
+    antinodes = set()
+    for positions in antena_positions.values():
+        for pairs in generate_antena_pairs(positions):
+            antinodes.update(get_valid_antinodes_v2(*pairs))
+    return len(antinodes)
 
 
 def main():
